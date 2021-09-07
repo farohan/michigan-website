da9e1fbec62fa08d6d43a029985648eeb278e6d9
farohan <68710283+farohan@users.noreply.github.com>
2021-09-06 17:42:24 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log(`outerHeight: ${window.outerHeight}`);
+console.log(`outerWidth: ${window.outerWidth}`);
+console.log(`innerHeight: ${window.innerHeight}`);
+console.log(`innerWidth: ${window.innerWidth}`);
+let browserHeight = window.innerHeight;
+console.log(`1vh = ${browserHeight / 100}px`);
