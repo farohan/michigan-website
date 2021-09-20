08530bea83e56e951a0517aa41f057e160993890
farohan <68710283+farohan@users.noreply.github.com>
2021-09-19 21:41:36 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,4 +3,20 @@ console.log(`outerWidth: ${window.outerWidth}`);
 console.log(`innerHeight: ${window.innerHeight}`);
 console.log(`innerWidth: ${window.innerWidth}`);
 let browserHeight = window.innerHeight;
-console.log(`1vh = ${browserHeight / 100}px`);
+console.log(`1vh = ${browserHeight / 100}px in this computer.`);
+
+//Scrolling enabled
+const downScroller = document.getElementById('down-scroller');
+const upScroller = document.getElementById('up-scroller');
+
+function slideScrollerDown() {
+    window.scrollBy(100, window.innerHeight);
+}
+
+downScroller.addEventListener('click', slideScrollerDown);
+
+function slideScrollerUp() {
+    window.scrollBy(100, -window.innerHeight);
+}
+
+upScroller.addEventListener('click', slideScrollerUp);
